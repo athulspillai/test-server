60c7de5761dfbef2ab4a2a406f97ae6a3b914197
athulspillai12334 <athulspillai2000@gmail.com>
2024-03-29 11:16:27 +0530

    imageupload

--- models/user.js
+++ models/user.js
@@ -25,6 +25,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    image: {
+        type: String,
+        required: true
+    },
     isAdmin: {
         type: Boolean,
         default: false,
