3cc37bce3420ffe617499616d76b360537c35f39
athulspillai12334 <athulspillai2000@gmail.com>
2024-03-31 21:37:06 +0530

    particularuser

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -2,16 +2,16 @@ import express from 'express'
 import UserController from '../controllers/login.js';
 import User from '../models/user.js';
 import multer from 'multer';
-
+import path from 'path';
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Destination folder for uploaded files
+        cb(null, 'uploads/images'); // Destination folder for uploaded files
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // Use the original file name
+        cb(null, Date.now() + path.extname(file.originalname)); // Use the original file name
     }
 });
 
