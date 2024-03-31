a655f0d6310d50ec06e6017064059bf904b4871d
athulspillai12334 <athulspillai2000@gmail.com>
2024-03-31 09:43:02 +0530

    imageupload

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -23,7 +23,7 @@ router.post('/update-location', UserController.UpdateLocation)
 
 router.get('/users', async (req, res) => {
     try {
-        const users = await User.find();z
+        const users = await User.find();
         res.status(200).json(users)
     } catch (error) {
         res.status(500).json({ message: 'Error while users details' })
